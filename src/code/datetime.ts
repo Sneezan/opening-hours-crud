9aa6a608e2ef3e7fa51c84beccd8fe21222c5abb
Sneezan <ceciliafraanlund@gmail.com>
2025-09-26 10:30:23 +0200

    cleaning up unused css

--- src/code/datetime.ts
+++ src/code/datetime.ts
@@ -1,5 +1,6 @@
 import { format, parse } from "date-fns";
 import type { Rules } from "./rules";
+import { RulePayload } from "../components/RuleEditor/Form/types";
 
 const DATE_FORMAT = "yyyy-MM-dd";
 const TIME_FORMAT = "HH:mm";
@@ -24,7 +25,7 @@ export const formatDate = (date: Date) => format(date, DATE_FORMAT);
 export const formatTime = (date: Date) => format(date, TIME_FORMAT);
 
 //  Format rules for clean logging
-export const formatRules = (rules: Rules<any>) =>
+export const formatRules = (rules: Rules<RulePayload>) =>
   console.log(
     rules.rules.map((data) => ({
       startDate: formatDate(data.startDate),
